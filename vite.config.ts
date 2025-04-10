b30c5cc5b8f5a28305b1d803ba9027e72ba22794
Leonardo Araujo <leonardoaraujo.oct@gmail.com>
2025-04-10 10:40:37 -0500

    Se cambio en conjunto para iniciar desde consola version gpt y uncp

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true, // Habilita Jest-like assertions (expect, toBe, etc.)
-    environment: "jsdom", // Simula el DOM en las pruebas
-    setupFiles: "./src/setupTests.ts", // Archivo de configuración
-    coverage: {
-      provider: "istanbul", // Opcional: Para reporte de cobertura de código
-    },
-  },
+	plugins: [react()],
+	test: {
+		globals: true, // Habilita Jest-like assertions (expect, toBe, etc.)
+		environment: "jsdom", // Simula el DOM en las pruebas
+		setupFiles: "./src/setupTests.ts", // Archivo de configuración
+		coverage: {
+			provider: "istanbul", // Opcional: Para reporte de cobertura de código
+		},
+	},
+	define: {
+		// Asegurarse de que las variables de entorno estén disponibles en el cliente
+		"import.meta.env.THEME": JSON.stringify(process.env.THEME),
+	},
 });
